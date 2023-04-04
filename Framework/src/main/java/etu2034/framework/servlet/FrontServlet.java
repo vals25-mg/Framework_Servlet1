2d325aa0de92c8df1c014c2fd80aababa31898d2
vals25-mg <rvalisoa3@gmail.com>
2023-04-04 06:46:22 +0300

    commit sprint1

---
+++ Framework/src/main/java/etu2034/framework/servlet/FrontServlet.java
@@ -0,0 +1,25 @@
+package etu2034.framework.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class FrontServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
